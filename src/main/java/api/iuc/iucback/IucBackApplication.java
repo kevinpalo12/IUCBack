fc2471bd127f6b7c82d2a2766077f01e3e184618
Kevin Palomino <49325499+kevinpalo12@users.noreply.github.com>
2020-11-19 14:33:39 -0500

    Initial commit

---
+++ src/main/java/api/iuc/iucback/IucBackApplication.java
@@ -0,0 +1,13 @@
+package api.iuc.iucback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IucBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IucBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/iuc/iucback/IucBackApplicationTests.java
@@ -0,0 +1,13 @@
+package api.iuc.iucback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IucBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
